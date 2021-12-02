c0039669bd57c1da9367a2f72defb6a567f61ec9
JoaoC <joaoc81@hotmail.com>
2021-12-02 19:48:36 +0000

    added should render the App component test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,14 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 import IntersectionObserver from "intersection-observer";
 
-test("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(<App />, div);
+describe("App", () => {
+  test("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(<App />, div);
+  });
+
+  it("should render the App component", () => {
+    render(<App />);
+    expect(screen.getByRole("heading")).toHaveTextContent("SpaceX");
+  });
 });
